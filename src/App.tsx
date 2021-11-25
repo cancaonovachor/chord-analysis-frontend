179a906ab6c065a134c4c359f3dd8ade47aeaf00
sunakane <20nm728a@vc.ibaraki.ac.jp>
2021-11-25 22:04:09 +0900

    UIをちょっとリッチにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
 import "./App.css";
 import { useForm } from "react-hook-form";
-import Dropzone from "./Dropzone";
+import axios from "axios";
+import { Button, Checkbox, Container, Stack } from "@mui/material";
+import { saveAs } from "file-saver";
 
 type FormType = {
   file: FileList;
@@ -25,22 +26,78 @@ const style = {
 function App() {
   const {
     register,
+    watch,
     formState: { errors },
     handleSubmit,
   } = useForm<FormType>();
-  const onSubmit = handleSubmit((data) => console.log(data.file));
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log("submit", data);
+    const d = new FormData();
+    d.append("file", data.file[0], data.file[0].name);
+    d.append("sameChordPass", data.sameChordPass ? "true" : "false");
+    await axios
+      .post(
+        "https://chord-analysis-backend-iq5232hggq-uc.a.run.app/upload",
+        // "localhost:8080/upload",
+        d,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      )
+      .then((response) => {
+        const blob = new Blob([response.data], {
+          type: response.data.type,
+        });
+        saveAs(blob, "result_" + data.file[0].name);
+      });
+  });
 
   return (
     <>
-      <h1>コード解析</h1>
-      <form method="post" encType="multipart/form-data" onSubmit={onSubmit}>
-        <Dropzone />
-        {errors.file && "ファイルはmusicxml形式でアップロードしてください"}
-        <br />
-        前と同じコードを飛ばす <input type="checkbox" name="sameChordPass" />
-        <br />
-        <input type="submit" value="Upload" />
-      </form>
+      <Container maxWidth="sm" sx={{ pt: 5 }}>
+        <h1 style={{ textAlign: "center" }}>コード解析</h1>
+        <Stack spacing={3}>
+          <form method="post" encType="multipart/form-data" onSubmit={onSubmit}>
+            {errors.file && "ファイルはmusicxml形式でアップロードしてください"}
+            <div>
+              <input
+                type="file"
+                {...register("file", { required: true })}
+                name="file"
+                id="fileid"
+                accept="*"
+                style={{ opacity: 0, appearance: "none", position: "absolute" }}
+              />
+              <label htmlFor="fileid">
+                <Button
+                  color="success"
+                  variant="contained"
+                  size="large"
+                  component="span"
+                >
+                  + クリックしてファイルを追加
+                </Button>
+              </label>
+            </div>
+            <div>
+              <label htmlFor="sameChordPassid">
+                <Checkbox id="sameChordPassid" {...register("sameChordPass")} />{" "}
+                前と同じコードを飛ばす
+              </label>
+            </div>
+
+            <div>
+              <input type="submit" style={{ display: "none" }} />
+              <label htmlFor="submitBtnid">
+                <Button color="primary" variant="contained" size="large">
+                  作成
+                </Button>
+              </label>
+            </div>
+          </form>
+        </Stack>
+      </Container>
     </>
   );
 }
